6c52059176ff81f7bab0242f07c741735fc0cec3
Queen's Table <castingcallforqueens@gmail.com>
2026-07-29 21:23:58 +0000

    File changes

--- src/components/coming-soon/ProductGallery.jsx
+++ src/components/coming-soon/ProductGallery.jsx
@@ -3,7 +3,7 @@ import { Bell, Check, Crown, PawPrint } from 'lucide-react';
 import { base44 } from '@/api/base44Client';
 
 const DROP4_IMG = 'https://media.base44.com/images/public/6a5a113aa6cf7e3091bf0eec/c3fdfff85_6bfda656-0039-48c5-b5b0-2b2d9e796443.png';
-const LANGUAGES_IMG = 'https://media.base44.com/images/public/6a5a113aa6cf7e3091bf0eec/006d07154_generated_image.png';
+const LANGUAGES_IMG = 'https://media.base44.com/images/public/6a5a113aa6cf7e3091bf0eec/6df07dbb7_38bf0d10-9897-4f90-81d3-0ee9b46ac015.png';
 const EMPOWERMENT_IMG = 'https://media.base44.com/images/public/6a5a113aa6cf7e3091bf0eec/9cac8a48c_Copilot_20260729_101403.png';
 const PETS_IMG = 'https://media.base44.com/images/public/6a5a113aa6cf7e3091bf0eec/96ea557da_generated_image.png';
 const TOTE_IMG = 'https://media.base44.com/images/public/6a5a113aa6cf7e3091bf0eec/92a01dd29_Copilot_20260729_125659.png';
