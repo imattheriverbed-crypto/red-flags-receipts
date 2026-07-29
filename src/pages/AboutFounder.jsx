b1c90290b379a5a2089191c536fe0037c5809056
Queen's Table <castingcallforqueens@gmail.com>
2026-07-29 21:53:54 +0000

    File changes

--- src/pages/AboutFounder.jsx
+++ src/pages/AboutFounder.jsx
@@ -7,7 +7,7 @@ import SiteFooter from '@/components/coming-soon/SiteFooter';
 import ScarfWeave from '@/components/coming-soon/ScarfWeave';
 import TickerTape from '@/components/coming-soon/TickerTape';
 
-const FOUNDER_IMG = 'https://media.base44.com/images/public/6a5a113aa6cf7e3091bf0eec/9741b8ed2_wm-30Yrs-Old-American-Escort-Brunette-Hazel-Eyes-D-Cup-Size-8-159CM-Tall-Long-Beach-CA-776362-4.jpg';
+const FOUNDER_IMG = 'https://media.base44.com/images/public/6a5a113aa6cf7e3091bf0eec/678d087d6_generated_image.png';
 
 export default function AboutFounder() {
   return (
